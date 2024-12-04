64a105ce9c0b0851ffab3fb54e3bc3616e1a0f41
ZainMunir <zainmunir999@gmail.com>
2024-12-04 11:32:36 +0100

    add: word based ordering

---
+++ a1/1a-fancy.py
@@ -0,0 +1,57 @@
+import sys
+from termcolor import colored
+import enchant
+
+dictionary = enchant.Dict("en_US")
+
+# input_file = "input-1a.txt"
+input_file = input(colored("Enter the input file: ", "green"))
+if not input_file:
+    print(colored("No input file", "red"))
+    sys.exit(1)
+
+cipher_text = ""
+with open(input_file, "r") as f:
+    cipher_text = f.read().lower()
+if not cipher_text:
+    print(colored("No cipher text", "red"))
+    sys.exit(1)
+
+filtered_cipher_text = "".join([c.lower() for c in cipher_text if c.isalpha()])
+cipher_text_len = len(filtered_cipher_text)
+
+similarity_scores = []
+
+for i in range(26):
+    decrypted_text = ""
+    for c in cipher_text:
+        if c.isalpha():
+            decrypted_text += chr((ord(c) - ord("a") - i) % 26 + ord("a"))
+        else:
+            decrypted_text += c
+    words = [x for x in decrypted_text.split(" ") if x]
+    num_words = len(words)
+    num_english_words = sum([dictionary.check(word) for word in words])
+    similarity_scores.append(num_english_words / num_words * 100)
+
+def nth_max_index(lst, n):
+    return sorted(range(len(lst)), key=lambda i: lst[i], reverse=True)[n]
+
+potential_keys = [
+    nth_max_index(similarity_scores, i) for i in range(len(similarity_scores))
+]
+
+for key in potential_keys:
+    decrypted_text = ""
+    for c in cipher_text:
+        if c.isalpha():
+            decrypted_text += chr((ord(c) - ord("a") - key) % 26 + ord("a"))
+        else:
+            decrypted_text += c
+    print(colored(f"Using key: {key}", "red"))
+    print(decrypted_text)
+    key_pressed = input(
+        colored("Enter 'q' to quit or any other key to continue: ", "green")
+    )
+    if key_pressed == "q":
+        break
